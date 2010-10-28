b39dde2d3993b9d36d20ee3760ce69498b595ad1
Victor Engmark <victor.engmark@gmail.com>
2010-10-29 00:52:54 +0200

    Clarified code for unpacking block format

--- mian/mian.py
+++ mian/mian.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""mian - Mine analysis - Graph blocks to height in a Minecraft save game
+"""mian - Mine analysis - Graph block types to height in a Minecraft save game
 <http://github.com/l0b0/mian>
 
 Default syntax:
 
-mian [-b|--blocks=<list>] [-l|--list] [-o|--output=filename] <World
-directory>
+mian [-b|--blocks=<list>] [-l|--list] [-o|--output=path] <World directory>
 
 Options:
 
--b, --blocks    Specify blocks to include as a comma-separated list, using
-                either the names or hex values from the list.
--l, --list      List available blocks (from
+-b, --blocks    Specify block types to include as a comma-separated list, using
+                either the block_names or hex values from the list.
+-l, --list      List available block types (from
                 <http://www.minecraftwiki.net/wiki/Data_values>).
 -o, --output    Output file name, defaults to WorldX.png.
 
@@ -27,10 +26,10 @@ $ mian ~/.minecraft/saves/World1
 Creates World1.png in the current directory with the graph.
 
 $ mian --blocks="diamond ore,mob spawner,obsidian" ~/.minecraft/saves/World1
-Ditto, showing only the specified blocks
+Ditto, showing only the specified block types
 
 $ mian --list
-Show a list of blocks that can be searched for
+Show a list of block types that can be searched for
 """
 
 __author__ = 'Pepijn de Vos, Victor Engmark'
@@ -44,8 +43,8 @@ from binascii import unhexlify
 from getopt import getopt, GetoptError
 from glob import glob
 import Gnuplot
-from itertools import izip_longest
-from nbt import nbt
+from itertools import izip
+from nbt.nbt import NBTFile
 from os.path import join, split
 from signal import signal, SIGPIPE, SIG_DFL
 from string import hexdigits
@@ -54,7 +53,7 @@ import warnings
 
 ARG_ERROR = 'You need to specify exactly one save directory.'
 
-BLOCKS = {
+BLOCK_TYPES = {
     'Air': '\x00',
     'Stone': '\x01',
     'Grass': '\x02',
@@ -142,7 +141,7 @@ BLOCKS = {
     'Jukebox': '\x54',
     'Fence': '\x55'}
 
-DEFAULT_BLOCKS = [
+DEFAULT_BLOCK_TYPES = [
     'Clay',
     'Coal ore',
     'Diamond ore',
@@ -151,102 +150,117 @@ DEFAULT_BLOCKS = [
     'Obsidian',
     'Redstone ore']
 
+CHUNK_SIZE_Y = 128
+CHUNK_SIZE_Z = 16
+CHUNK_SIZE_X = CHUNK_SIZE_Y * CHUNK_SIZE_Z
+
 signal(SIGPIPE, SIG_DFL)
 """Avoid 'Broken pipe' message when canceling piped command."""
 
 
-def _lookup_block(block):
+def _lookup_block_type(block_type):
     """
-    Find blocks based on input string.
+    Find block types based on input string.
     
-    @param block: Name or hex ID of a block.
-    @return: Subset of BLOCKS.
+    @param block_type: Name or hex ID of a block type.
+    @return: Subset of BLOCK_TYPES.
     
     Examples:
-    >>> sorted(_lookup_block('gold'))
+    >>> sorted(_lookup_block_type('gold'))
     ['Gold block', 'Gold ore']
-    >>> _lookup_block('20')
+    >>> _lookup_block_type('20')
     {'Magenta cloth': ' '}
     """
 
-    if block is None or len(block) == 0:
-        warnings.warn('Empty block')
+    if block_type is None or len(block_type) == 0:
+        warnings.warn('Empty block type')
         return None
 
-    block = block.lower()
+    block_type = block_type.lower()
 
-    if len(block) == 2 and all(char in hexdigits for char in block):
-        # Look up single block by hex value
-        for key, value in BLOCKS.iteritems():
-            if value == unhexlify(block):
+    if len(block_type) == 2 and all(char in hexdigits for char in block_type):
+        # Look up single block type by hex value
+        for key, value in BLOCK_TYPES.iteritems():
+            if value == unhexlify(block_type):
                 return {key: value}
 
     # Name substring search
     result = {}
-    for name in sorted(BLOCKS):
-        if name.lower().find(block) != -1:
-            result[name] = BLOCKS[name]
+    for name in sorted(BLOCK_TYPES):
+        if name.lower().find(block_type) != -1:
+            result[name] = BLOCK_TYPES[name]
 
     if result == []:
-        warnings.warn('Unknown block %s' % block)
+        warnings.warn('Unknown block type %s' % block_type)
     return result
 
 
-def print_blocks():
-    """Print the block names and hexadecimal IDs"""
-    max_length = len(max(BLOCKS.keys(), key=len))
-    for name in sorted(BLOCKS):
+def print_block_types():
+    """Print the block block_names and hexadecimal IDs"""
+    max_length = len(max(BLOCK_TYPES.keys(), key=len))
+    for name in sorted(BLOCK_TYPES):
         print \
             name.ljust(max_length),\
-            hex(ord(BLOCKS[name]))[2:].upper().zfill(2)
+            hex(ord(BLOCK_TYPES[name]))[2:].upper().zfill(2)
 
 
-def mian(world_dir, output_file, blocks):
+def mian(world_dir, output_file, block_types):
     """
-    Runs through the files and creates the output.
+    Runs through the DAT files and creates the output.
     
     @param world_dir: Path to existing Minecraft world directory.
     @param output_file: Path to file which should be written.
-    @param blocks: Subset of BLOCKS.
+    @param block_types: Subset of BLOCK_TYPES.
     """
-    files = glob(join(world_dir, '*/*/*.dat')) # All world blocks
+    paths = glob(join(world_dir, '*/*/*.dat')) # All world blocks
 
-    col = []
+    xyz_values = []
 
-    for file in files:
-        #print file
-        file = nbt.NBTFile(file,'rb')
+    # Unpack block format
+    # <http://www.minecraftwiki.net/wiki/Alpha_Level_Format#Block_Format>
+    for path in paths:
+        nbtfile = NBTFile(path,'rb')
+        
+        yzx_block = nbtfile["Level"]["Blocks"].value
+        
+        yzx_block_list = [iter(yzx_block)]
+        
+        xyz_block = izip(
+                *yzx_block_list * CHUNK_SIZE_Y)
 
-        col.extend(izip_longest(*[iter(file["Level"]["Blocks"].value)]*128))
+        xyz_values.extend(xyz_block)
 
-        file.file.close()
+        nbtfile.file.close()
 
-    layers = zip(*col)
+    zxy_values = izip(*xyz_values)
 
-    names = blocks.keys()
-    hexes = blocks.values()
+    bt_names = block_types.keys()
+    bt_hexes = block_types.values()
 
-    def count_blocks(layer):
+    def count_block_types(layer):
         def filter_block(bname):
-            count = len([block for block in layer if block == bname])
-            return count
+            return len([block for block in layer if block == bname])
 
-        m = map(filter_block, hexes)
-        return m
+        return map(filter_block, bt_hexes)
+
+    y_counts = []
+    for layer in zxy_values:
+        y_counts.append(count_block_types(layer))
+    
+    data = izip(*y_counts)
+    print data
 
     gnuplot = Gnuplot.Gnuplot()
     gnuplot('set term png')
     gnuplot('set out "%s"' % output_file)
     gnuplot('set style data lines')
-
-    data = zip(*(count_blocks(layer) for layer in layers))
-
-    data = (
+    
+    plot_data = (
         Gnuplot.PlotItems.Data(
             list(enumerate(block)),
-            title=names[index]) for index, block in enumerate(data))
+            title=bt_names[index]) for index, block in enumerate(data))
 
-    gnuplot.plot(*data)
+    gnuplot.plot(*plot_data)
 
 
 def main(argv = None):
@@ -256,7 +270,7 @@ def main(argv = None):
         argv = sys.argv
 
     # Defaults
-    block_names = DEFAULT_BLOCKS
+    block_block_names = DEFAULT_BLOCK_TYPES
     output_file = None
 
     try:
@@ -270,9 +284,9 @@ def main(argv = None):
 
     for option, value in opts:
         if option in ('-b', '--blocks'):
-            block_names = value.split(',')
+            block_block_names = value.split(',')
         elif option in ('-l', '--list'):
-            print_blocks()
+            print_block_types()
             return 0
         elif option in ('-o', '--output'):
             output_file = value
@@ -284,12 +298,12 @@ def main(argv = None):
         # Use savegame directory name
         output_file = split(world_dir)[1] + '.png'
 
-    # Look up blocks
-    blocks = {}
-    for name in block_names:
-        blocks.update(_lookup_block(name))
+    # Look up block_types
+    block_types = {}
+    for name in block_block_names:
+        block_types.update(_lookup_block_type(name))
 
-    mian(world_dir, output_file, blocks)
+    mian(world_dir, output_file, block_types)
 
 
 if __name__ == '__main__':
