e69954bfcd6f0409935e048c3e202e8a82e8fed9
Victor Engmark <victor.engmark@gmail.com>
2011-01-20 21:35:22 +0100

    Reformatted to unify display

--- mian/blocks.py
+++ mian/blocks.py
@@ -8,110 +8,191 @@ UNUSED_NAME = '<unused>'
 # The first name is the canocical one (for the moment).
 # Subsequent names are synonyms.
 BLOCK_TYPES = {
-    '\x00': ['Air'],
-    '\x01': ['Stone'],
-    '\x02': ['Grass'],
-    '\x03': ['Dirt'],
-    '\x04': ['Cobblestone'],
+    '\x00': [
+        'Air'],
+    '\x01': [
+        'Stone'],
+    '\x02': [
+        'Grass'],
+    '\x03': [
+        'Dirt'],
+    '\x04': [
+        'Cobblestone'],
     '\x05': [
+        'Wooden plank',
         'Wood',
         'Log'],
-    '\x06': ['Sapling'],
+    '\x06': [
+        'Sapling'],
     '\x07': [
         'Bedrock',
         'Adminium'],
-    '\x08': ['Water'],
-    '\x09': ['Stationary water'],
-    '\x0a': ['Lava'],
-    '\x0b': ['Stationary lava'],
-    '\x0c': ['Sand'],
-    '\x0d': ['Gravel'],
-    '\x0e': ['Gold ore'],
-    '\x0f': ['Iron ore'],
-    '\x10': ['Coal ore'],
-    '\x11': ['Log'],
-    '\x12': ['Leaves'],
-    '\x13': ['Sponge'],
-    '\x14': ['Glass'],
-    '\x15': ['Red cloth'],
-    '\x16': ['Orange cloth'],
-    '\x17': ['Yellow cloth'],
-    '\x18': ['Lime cloth'],
-    '\x19': ['Green cloth'],
-    '\x1a': ['Aqua green cloth'],
-    '\x1b': ['Cyan cloth'],
-    '\x1c': ['Blue cloth'],
-    '\x1d': ['Purple cloth'],
-    '\x1e': ['Indigo cloth'],
-    '\x1f': ['Violet cloth'],
-    '\x20': ['Magenta cloth'],
-    '\x21': ['Pink cloth'],
-    '\x22': ['Black cloth'],
-    '\x23': ['Gray / white cloth'],
-    '\x24': ['White cloth'],
-    '\x25': ['Yellow flower'],
-    '\x26': ['Red rose'],
-    '\x27': ['Brown mushroom'],
-    '\x28': ['Red mushroom'],
-    '\x29': ['Gold block'],
-    '\x2a': ['Iron block'],
+    '\x08': [
+        'Water'],
+    '\x09': [
+        'Stationary water'],
+    '\x0a': [
+        'Lava'],
+    '\x0b': [
+        'Stationary lava'],
+    '\x0c': [
+        'Sand'],
+    '\x0d': [
+        'Gravel'],
+    '\x0e': [
+        'Gold ore'],
+    '\x0f': [
+        'Iron ore'],
+    '\x10': [
+        'Coal ore'],
+    '\x11': [
+        'Wood',
+        'Log'],
+    '\x12': [
+        'Leaves'],
+    '\x13': [
+        'Sponge'],
+    '\x14': [
+        'Glass'],
+    '\x15': [
+        'Red cloth'],
+    '\x16': [
+        'Orange cloth'],
+    '\x17': [
+        'Yellow cloth'],
+    '\x18': [
+        'Lime cloth'],
+    '\x19': [
+        'Green cloth'],
+    '\x1a': [
+        'Aqua green cloth'],
+    '\x1b': [
+        'Cyan cloth'],
+    '\x1c': [
+        'Blue cloth'],
+    '\x1d': [
+        'Purple cloth'],
+    '\x1e': [
+        'Indigo cloth'],
+    '\x1f': [
+        'Violet cloth'],
+    '\x20': [
+        'Magenta cloth'],
+    '\x21': [
+        'Pink cloth'],
+    '\x22': [
+        'Black cloth'],
+    '\x23': [
+        'Gray / white cloth'],
+    '\x24': [
+        'White cloth'],
+    '\x25': [
+        'Yellow flower'],
+    '\x26': [
+        'Red rose'],
+    '\x27': [
+        'Brown mushroom'],
+    '\x28': [
+        'Red mushroom'],
+    '\x29': [
+        'Gold block'],
+    '\x2a': [
+        'Iron block'],
     '\x2b': [
         'Double slab',
         'Double step'],
     '\x2c': [
         'Stone slab',
         'Step'],
-    '\x2d': ['Brick'],
-    '\x2e': ['TNT'],
-    '\x2f': ['Bookshelf'],
+    '\x2d': [
+        'Brick'],
+    '\x2e': [
+        'TNT'],
+    '\x2f': [
+        'Bookshelf'],
     '\x30': [
         'Moss stone',
         'Mossy cobblestone'],
-    '\x31': ['Obsidian'],
-    '\x32': ['Torch'],
-    '\x33': ['Fire'],
+    '\x31': [
+        'Obsidian'],
+    '\x32': [
+        'Torch'],
+    '\x33': [
+        'Fire'],
     '\x34': [
         'Monster spawner',
         'Mob spawner'],
-    '\x35': ['Wooden stairs'],
-    '\x36': ['Chest'],
-    '\x37': ['Redstone wire'],
-    '\x38': ['Diamond ore'],
-    '\x39': ['Diamond block'],
-    '\x3a': ['Workbench'],
-    '\x3b': ['Crops'],
-    '\x3c': ['Soil'],
-    '\x3d': ['Furnace'],
-    '\x3e': ['Burning furnace'],
-    '\x3f': ['Sign post'],
-    '\x40': ['Wooden door'],
-    '\x41': ['Ladder'],
+    '\x35': [
+        'Wooden stairs'],
+    '\x36': [
+        'Chest'],
+    '\x37': [
+        'Redstone wire'],
+    '\x38': [
+        'Diamond ore'],
+    '\x39': [
+        'Diamond block'],
+    '\x3a': [
+        'Workbench'],
+    '\x3b': [
+        'Crops'],
+    '\x3c': [
+        'Soil'],
+    '\x3d': [
+        'Furnace'],
+    '\x3e': [
+        'Burning furnace'],
+    '\x3f': [
+        'Sign post'],
+    '\x40': [
+        'Wooden door'],
+    '\x41': [
+        'Ladder'],
     '\x42': [
         'Minecart tracks',
         'Rails'],
-    '\x43': ['Cobblestone stairs'],
-    '\x44': ['Wall sign'],
-    '\x45': ['Lever'],
-    '\x46': ['Stone pressure plate'],
-    '\x47': ['Iron door'],
-    '\x48': ['Wooden pressure plate'],
-    '\x49': ['Redstone ore'],
-    '\x4a': ['Glowing redstone ore'],
-    '\x4b': ['Redstone torch [off]'],
-    '\x4c': ['Redstone torch [on]'],
-    '\x4d': ['Stone button'],
-    '\x4e': ['Snow'],
-    '\x4f': ['Ice'],
-    '\x50': ['Snow block'],
-    '\x51': ['Cactus'],
-    '\x52': ['Clay'],
+    '\x43': [
+        'Cobblestone stairs'],
+    '\x44': [
+        'Wall sign'],
+    '\x45': [
+        'Lever'],
+    '\x46': [
+        'Stone pressure plate'],
+    '\x47': [
+        'Iron door'],
+    '\x48': [
+        'Wooden pressure plate'],
+    '\x49': [
+        'Redstone ore'],
+    '\x4a': [
+        'Glowing redstone ore'],
+    '\x4b': [
+        'Redstone torch [off]'],
+    '\x4c': [
+        'Redstone torch [on]'],
+    '\x4d': [
+        'Stone button'],
+    '\x4e': [
+        'Snow'],
+    '\x4f': [
+        'Ice'],
+    '\x50': [
+        'Snow block'],
+    '\x51': [
+        'Cactus'],
+    '\x52': [
+        'Clay'],
     '\x53': [
         'Reed',
         'Bamboo',
         'Papyrus'],
-    '\x54': ['Jukebox'],
-    '\x55': ['Fence'],
-    '\x56': ['Pumpkin'],
+    '\x54': [
+        'Jukebox'],
+    '\x55': [
+        'Fence'],
+    '\x56': [
+        'Pumpkin'],
     '\x57': [
         'Netherrack',
         'Bloodstone',
@@ -130,9 +211,12 @@ BLOCK_TYPES = {
         'Brightstone',
         'Australium',
         'Brimstone'],
-    '\x5a': ['Portal'],
-    '\x5b': ['Jack-o-lantern'],
-    '\x5c': ['Cake block'],
+    '\x5a': [
+        'Portal'],
+    '\x5b': [
+        'Jack-o-lantern'],
+    '\x5c': [
+        'Cake block'],
     '\x5d': [UNUSED_NAME],
     '\x5e': [UNUSED_NAME],
     '\x5f': [UNUSED_NAME],
