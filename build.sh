5b7365f671094f54bfb52dd05b29ebff81ae1ab6
Victor Engmark <victor.engmark@gmail.com>
2011-01-27 22:50:00 +0100

    Automatic tagging

--- build.sh
+++ build.sh
@@ -13,3 +13,6 @@ python setup.py ${1:-} bdist_egg bdist_rpm sdist upload
 # Cleanup
 python setup.py clean
 rm -fr *.pyc build dist *.egg-info
+
+# Add release tag
+git tag -a -m 'PyPI release' "$(PYTHONPATH=. python -c 'from mian.mian import __version__; print __version__')"
