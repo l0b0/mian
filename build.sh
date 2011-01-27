cd915a786363cf997620a773489a53969341a67d
Victor Engmark <victor.engmark@gmail.com>
2011-01-27 22:49:49 +0100

    Safer build
    Exits on first error instead of using && everywhere

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-# Requires rpm package
-# Reintroduce bdist_wininst when Windows building works again
-cd $(dirname $0) && \
-python setup.py test && \
-python setup.py $1 bdist_egg bdist_rpm sdist upload clean && \
+set -o errexit -o nounset
+
+cd -- "$(dirname -- "$(readlink -fn -- "$0")")"
+
+# Test
+python setup.py test
+
+# Build
+python setup.py ${1:-} bdist_egg bdist_rpm sdist upload
+
+# Cleanup
+python setup.py clean
 rm -fr *.pyc build dist *.egg-info
